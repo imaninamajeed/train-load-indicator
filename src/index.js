aba50293c9bfd2ba5d80e348a548328f65b461af
imaninamajeed <imaninamajeed97@gmail.com>
2024-05-25 12:19:17 +0800

    refactor: Update routing

--- src/index.js
+++ src/index.js
@@ -2,12 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import App from "./App";
-import Login from "./components/Login";
+import Login from "./pages/Login/Login";
+import Dashboard from "./pages/Dashboard/Dashboard";
 // import Register from "./components/Register";
-import PrivateRoute from "./components/PrivateRoute";
+import PrivateRoute from "./routing/PrivateRoute";
 import "./index.css";
-import "@fontsource/source-sans-pro"; // Defaults to weight 400
-
+import "bootstrap/dist/css/bootstrap.min.css";
+import "@fontsource/poppins"; // Defaults to weight 400
 
 ReactDOM.render(
 	<Router>
@@ -16,6 +17,7 @@ ReactDOM.render(
 			{/* <Route path="/register" element={<Register />} /> */}
 			<Route element={<PrivateRoute />}>
 				<Route path="/" element={<App />} />
+				<Route path="/dashboard" element={<Dashboard />} />
 			</Route>
 		</Routes>
 	</Router>,
